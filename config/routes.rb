b16e3a3caa39e929dfc84e338b819c0ae6da0b8a
yutaka18 <ibayutaka@aibayutakanoMacBook-puro.local>
2018-10-10 17:25:27 +0900

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
     root 'blogers#index'
     get   'blogs'      =>  'blogers#index'
-    get 'blogs/new' => 'blogers#new'     
-    post 'blogs' => 'blogers#create'  
-    get 'users/:id' => 'users#show'    
-    delete 'blogs/:id' => 'blogers#destroy'  
-    get 'blogs/:id/edit' => 'blogers#edit' 
+    get 'blogs/new' => 'blogers#new'
+    post 'blogs' => 'blogers#create'
+    get 'users/:id' => 'users#show'
+    delete 'blogs/:id' => 'blogers#destroy'
+    get 'blogs/:id/edit' => 'blogers#edit'
     patch 'blogs/:id'  => 'blogers#update'
-    get 'blogs/:id' => 'blogers#show'  
+    get 'blogs/:id' => 'blogers#show'
 
 
 
